f2dfa17916fad72766025001bd9a305e77cbaceb
mkhan503 <mahah.mujeebkhan@gmail.com>
2021-08-30 01:20:20 +0400

    Code done. Move to step 4

--- external.js
+++ external.js
@@ -1,4 +1,7 @@
-game();
+
+//Adding eventlisteners to buttons, so that game starts off when a button is clicked 
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button)=> button.addEventListener('click', playRound));
 
 function computerPlay(){
     //Creat Array of three words//
@@ -13,94 +16,47 @@ function computerPlay(){
 }
 
 
-function playRound(personChoice, computerChoice){
+function playRound(e){
+    const result = document.querySelector('#result');
+    console.log(result);
 
-    if (personChoice != "rock" && personChoice != "paper" && personChoice != "scissors"){
-        return "Incorrect option";
-    }
+    const personChoice = e.target.id;
+    const personSelect = document.querySelector('#choicePerson');
+    console.log(personSelect);
+    personSelect.textContent = `You chose: ${personChoice}`;
+   
+    
+    // Displaying computer choice on screen instead of console
+    const computerSelect = document.querySelector('#choiceComputer');
+    const computerChoice = computerPlay();
+    computerSelect.textContent = `Computer chose: ${computerChoice}`;
+   
+
+
+
     //if personChoice and compterChoice are same return Draw//
-    else if (personChoice == computerChoice){
-        return "Draw";
+    if (personChoice == computerChoice){
+        result.textContent = 'Draw';
         
     }  
     //elseif PC = rock & CC = scissors, return you win//
     else if(personChoice == "rock" && computerChoice == "scissors"){
-        return "You win, rock beats scissors";
+        result.textContent = 'You win, rock beats scissors';
     }
     //elseif PC = paper & CC = rock, return you win//
     else if(personChoice == "paper" && computerChoice == "rock"){
-        return "You win paper beats rock";
+        result.textContent = 'You win paper beats rock';
     }
     // elseif PC = scissors & CC = paper return you win//
     else if(personChoice == "scissors" && computerChoice == "paper"){
-        return "You win, scissors beat paper";
+        result.textContent = 'You win, scissors beat paper';
     }
     //else return you lose//
     else{
-        return "You lose! " + computerChoice + " beats " + personChoice;
-
+        result.textContent = `You lose! ${computerChoice} beats ${personChoice}`;
     }
     
 }
 
-function game(){
-    let computerScore = 0;
-    let personScore = 0;
-    //play 5 games calling playRound function, adding scores//
-    // for draw, no score is added//
-    for (let i = 0; i < 5; i++){
-        
-            let personChoice = window.prompt("Select one of: rock, paper, scissors","");
-            
-            personChoice = personChoice.toLowerCase();
-            console.log(personChoice);
-            
-            let computerChoice = computerPlay();
-            console.log(computerChoice);
-
-            const result = playRound(personChoice, computerChoice);
-        
-            
-            if (result.includes("win")){
-            personScore += 1;
-            console.log(personScore);
-            }
-            else if (result.includes("lose")){
-                computerScore += 1;
-                console.log(computerScore);
-            }
-
-            //if incorrect spelling entered, rerun the round//
-
-            else if (result.includes("Incorrect")){
-                i =- 1;
-            }
-
-            alert(result);
-
-        
-        
-
-    }
-    //at the end of for loop compare computerScore with personScore//
-    //announce the winner//
-
-    alert ("Person score = " + personScore +
-    " Computer score = " + computerScore);
-   
-
-    if (personScore > computerScore){
-        alert("You win!!");
-    }
-    else if (personScore < computerScore){
-        alert("You lose!");
-    }
-    else{
-        alert("It's a draw!");
-    }
+  
     
-}
-
-
-
-
